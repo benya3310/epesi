44552710ff9c98230656834e276e26629070e6d1
Paul Bukowski <shacky7@gmail.com>
2009-09-28 20:24:41 +0200

    cleanup

--- admin/phpinfo.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once('auth.php');
-phpinfo();
-print('<hr><a href="index.php">back</a>');
-?>
--- map.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-global $virtual_hosts;
-$virtual_hosts = array(
-	'/^http:\/\/localhost\/epesi/' => false, //deny http://localhost/epesi
-	'/^/' => 'data' //allow http://192.168.13.10/epesi
-);*/
-?>
